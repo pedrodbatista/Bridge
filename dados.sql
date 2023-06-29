43588cc6bfd0471fd9d290a68ed17794c0f671d6
dani <dani.modesti@usp.br>
2023-06-29 16:39:15 -0300

    Adicionando dados à base

---
+++ dados.sql
@@ -0,0 +1,75 @@
+------------------------------------------------------------------------------------------------
+-- |                               POPULACAO DA BASE DE DADOS                               | --
+------------------------------------------------------------------------------------------------
+
+-- Carregando dados para a tabela ASSISTIDO
+
+
+
+-- Carregando dados para a tabela UNIDADE
+
+
+
+-- Carregando dados para a tabela TREINAMENTO
+
+
+
+-- Carregando dados para a tabela FUNCIONARIO
+
+
+
+-- Carregando dados para a tabela FUNCIONARIO_CARGO
+
+
+
+-- Carregando dados para a tabela ADMINISTRADOR
+
+
+
+-- Carregando dados para a tabela PROFESSOR
+
+
+
+-- Carregando dados para a tabela MONITOR
+
+
+
+-- Carregando dados para a tabela SALA
+
+
+
+-- Carregando dados para a tabela LABORATORIO
+
+
+
+-- Carregando dados para a tabela SALA_DE_AULA
+
+
+
+-- Carregando dados para a tabela EQUIPAMENTO
+
+
+
+-- Carregando dados para a tabela OFERECIMENTO
+
+
+
+-- Carregando dados para a tabela AGENDAMENTO
+
+
+
+-- Carregando dados para a tabela INSCREVE_SE_EM
+
+
+
+-- Carregando dados para a tabela CERTIFICA
+
+
+
+-- Carregando dados para a tabela MONITORIA
+
+
+
+-- Carregando dados para a tabela SOLICITACAO_DOACAO
+
+
